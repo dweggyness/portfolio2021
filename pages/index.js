071fc1fea01e8a057032db5f912db33dce35b06a
dweggy <colacolaojm@hotmail.com>
2021-07-30 04:31:02 +0800

    add down arrow at bottom of page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IoIosArrowDown } from 'react-icons/io';
 import styles from '../styles/Home.module.css';
 import Header from '../components/Header';
 
@@ -6,16 +7,20 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
-      <div className={styles.frontPage}>
+      <section className={styles.frontPage}>
         <h2 className={styles.frontTitle}>Hello!</h2>
         <p className={styles.frontDescription}>
           I'm Jun, a <span className={styles.highlight}>Malaysian</span> sophomore at
           <span className={styles.highlight}> NYU Abu Dhabi.</span>
         </p>
         <p className={styles.frontDescription}>
-          Interested in <span className={styles.highlight}>Computer Science</span>, Design, accompanied with plenty of tea.
+          Interested in <span className={styles.highlight}>Computer Science</span>,
+          Design, accompanied with plenty of tea.
         </p>
-      </div>
+        <div className={styles.downArrowContainer}>
+          <IoIosArrowDown size='1.4em'/>
+        </div>
+      </section>
     </div>
   );
 }
