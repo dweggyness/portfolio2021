12d8e09ded99532c28ceb05808ca2d19eecffe89
dweggy <colacolaojm@hotmail.com>
2021-07-30 03:47:03 +0800

    header css changes, add projects page

--- components/Header.js
+++ components/Header.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/Header.module.css';
 import avatar from '../public/avatar.jpg';
 
 export default function Header() {
   return (
     <div className={styles.header}>
-      <div>
-        <Image
-          className={styles.avatar}
-          src={avatar}
-          alt='Picture of myself'
-          width={'60px'}
-          height={'60px'}
-        />
-      </div>
-      <div className={styles.socialIconsBar}>
-        <a className={styles.socialIconContainer} href='https://github.com/dweggyness/'>
+      <Link href="/">
+        <a>
+          <Image
+            className={styles.avatar}
+            src={avatar}
+            alt='Picture of myself'
+            width={'60px'}
+            height={'60px'}
+          />
+        </a>
+      </Link>
+      <div className={styles.headerRightBar}>
+        <Link href="/projects">
+          <a
+            className={styles.underlineOnHover}
+            style={{ fontSize: '1.2em', verticalAlign: 'middle', bottom: '2.5px' }}>
+            <span>
+              Projects
+            </span>
+          </a>
+        </Link>
+        <a className={styles.underlineOnHover} href='https://github.com/dweggyness/'>
           <FaGithub className={styles.socialIcon} size='1.4em'/>
         </a>
-        <a className={styles.socialIconContainer} href='https://www.linkedin.com/in/ooi-jun-ming-1695251a9/'>
+        <a className={styles.underlineOnHover} href='https://www.linkedin.com/in/ooi-jun-ming-1695251a9/'>
           <FaLinkedin className={styles.socialIcon} size='1.4em'/>
         </a>
-        <a className={styles.socialIconContainer} href='https://www.instagram.com/ojmming/'>
+        <a className={styles.underlineOnHover} href='https://www.instagram.com/ojmming/'>
           <FaInstagram className={styles.socialIcon} size='1.4em'/>
         </a>
       </div>
