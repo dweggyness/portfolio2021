ba70bce0f069437867d1f65ca4011055dc454981
dweggy <colacolaojm@hotmail.com>
2021-07-30 04:16:46 +0800

    add front page title text

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,16 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
-      Hello World!
+      <div className={styles.frontPage}>
+        <h2 className={styles.frontTitle}>Hello!</h2>
+        <p className={styles.frontDescription}>
+          I'm Jun, a <span className={styles.highlight}>Malaysian</span> sophomore at
+          <span className={styles.highlight}> NYU Abu Dhabi.</span>
+        </p>
+        <p className={styles.frontDescription}>
+          Interested in <span className={styles.highlight}>Computer Science</span>, Design, accompanied with plenty of tea.
+        </p>
+      </div>
     </div>
   );
 }
