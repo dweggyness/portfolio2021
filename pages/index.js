0db606b4fd3146fa860bd669d2e3dcbae17b5e10
dweggy <colacolaojm@hotmail.com>
2021-08-01 19:42:46 +0800

    css for home page - made down arrow stick to bottom of page

--- pages/index.js
+++ pages/index.js
@@ -6,20 +6,25 @@ import Header from '../components/Header';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Header />
       <section className={styles.frontPage}>
-        <h2 className={styles.frontTitle}>Hello!</h2>
-        <p className={styles.frontDescription}>
-          I'm Jun, a <span className={styles.highlight}>Malaysian</span> sophomore at
-          <span className={styles.highlight}> NYU Abu Dhabi.</span>
-        </p>
-        <p className={styles.frontDescription}>
-          Interested in <span className={styles.highlight}>Computer Science</span>,
-          Design, accompanied with plenty of tea.
-        </p>
+        <Header />
+        <section className={styles.titleContainer}>
+          <h2 className={styles.frontTitle}>Hello!</h2>
+          <p className={styles.frontDescription}>
+            I'm Jun, a <span className={styles.highlight}>Malaysian</span> sophomore at
+            <span className={styles.highlight}> NYU Abu Dhabi.</span>
+          </p>
+          <p className={styles.frontDescription}>
+            Interested in <span className={styles.highlight}>Computer Science</span>,
+            Design, accompanied with plenty of tea.
+          </p>
+        </section>
         <div className={styles.downArrowContainer}>
           <IoIosArrowDown size='1.4em'/>
         </div>
+      </section>
+      <section className={styles.experienceContainer}>
+        <h3 className={styles.contentTitle}>Work</h3>
       </section>
     </div>
   );
