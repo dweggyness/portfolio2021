b22b1184e40a1551709bfefe6ad5a55c09837311
dweggy <colacolaojm@hotmail.com>
2021-08-02 19:27:40 +0800

    add contact me button to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { MdEmail } from 'react-icons/md';
 import styles from '../styles/Footer.module.css';
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
       Jun Ming © 2021-2021
-      <a href="mailto:jmo460@nyu.edu">
+      <div style={{ marginLeft: '0.8em' }} />
+      •
+      <div style={{ marginLeft: '0.9em' }} />
+      <a className={styles.contactButton} href="https://aemail.com/gJm9">
+        <MdEmail />
+        <div style={{ marginLeft: '0.2em' }} />
         Contact me!
       </a>
     </div>
