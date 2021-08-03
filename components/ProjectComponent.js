d577965e1c8261bfa3c1195f6165c4e944cd1e15
dweggy <colacolaojm@hotmail.com>
2021-08-03 21:56:29 +0800

    complete Projectcomponent

--- components/ProjectComponent.js
+++ components/ProjectComponent.js
@@ -3,52 +3,60 @@ import PropTypes from 'prop-types';
 import Image from 'next/image';
 import { IoToday, IoLogoGithub } from 'react-icons/io5';
 import styles from '../styles/components/ProjectComponent.module.css';
-import Dwegstea from '../public/projects/dwegstea.png';
 
 export default function ProjectComponent(props) {
-  const { img, title = 'Title', link = 'www.google.com', githubLink = 'd' } = props;
-  const description = "This project is about something, using something."
-  const techStackText = 'React/JS/CSS/HTML';
-  const date = 'Aug 2020';
+  const { img, title, projectLink, githubLink, description, techStackText, date } = props;
   return (
     <section className={`flexCol ${styles.container}`}>
       <div className={'flex'}>
         <div className={`flexCol ${styles.descriptionContainerLeft}`}>
           <div style={{ position: 'relative' }}>
-            <a className={styles.hoverAnim} href={link} target='_blank' rel='noreferrer'>
+            <a className={styles.hoverAnim} href={projectLink} target='_blank' rel='noreferrer'>
               <h3 className={styles.title}>{title}</h3>
             </a>
           </div>
-          <p className={styles.description}>{description}</p>
-          <p className={styles.techStackText}>{techStackText}</p>
+          <div style={{ marginBottom: '0.2em' }} />
+          <p>{description}</p>
+          <p className={'lightText'}>{techStackText}</p>
         </div>
         <div className={`flexCol ${styles.descriptionContainerRight}`}>
-          <div className={'flex alignCenter'}>
-            <p style={{ color: 'rgba(255, 255, 255, 0.65)' }}>{date}</p>
+          <div className={'flex alignCenter lightText'} >
+            <IoToday />
+            <div style={{ marginLeft: '0.4em' }} />
+            <p>{date}</p>
           </div>
           <div className={'flex alignCenter'}>
             <div style={{ position: 'relative' }}>
-              <a className={`flex alignCenter ${styles.hoverAnim}`} href={githubLink} target='_blank' rel='noreferrer'>
+              <a className={`flex alignCenter ${styles.githubLink}`} href={githubLink}>
                 <IoLogoGithub style={{ marginTop: '1px' }} />
                 <div style={{ marginLeft: '0.4em' }} />
-                <p>Github Link</p>
+                <p>Github</p>
               </a>
             </div>
           </div>
         </div>
       </div>
       <div style={{ marginTop: '1em' }} />
-      <div className={styles.projectImage}>
-        <Image
-          src={Dwegstea}
-          alt='project image'
-          layout='fill'
-          objectFit='contain'
-        />
+      <div className={styles.projectImageContainer}>
+        <a href={projectLink}>
+          <Image
+            className={styles.projectImage}
+            src={img}
+            alt='project image screenshot'
+            objectFit='contain'
+          />
+        </a>
       </div>
     </section>
   );
 }
 
 ProjectComponent.propTypes = {
+  img: PropTypes.element,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  githubLink: PropTypes.string,
+  projectLink: PropTypes.string,
+  techStackText: PropTypes.string,
+  date: PropTypes.string,
 };
