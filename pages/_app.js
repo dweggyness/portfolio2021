8a618173dd28a24a85cac45bdfe6947382a2a415
dweggy <colacolaojm@hotmail.com>
2021-08-04 11:24:02 +0800

    add title to website

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Head from 'next/head';
 import PropTypes from 'prop-types';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Jun Ming</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 MyApp.propTypes = {
